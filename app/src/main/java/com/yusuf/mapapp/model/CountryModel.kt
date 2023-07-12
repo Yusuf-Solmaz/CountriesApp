b8df3ab2126d8dc9b59a018603179088b21e609f
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2023-07-12 19:49:48 +0300

    Country Data Class

--- app/src/main/java/com/yusuf/mapapp/model/CountryModel.kt
+++ app/src/main/java/com/yusuf/mapapp/model/CountryModel.kt
@@ -1,2 +1,10 @@
 package com.yusuf.mapapp.model
 
+data class CountryModel(
+    val countryName : String?,
+    val countryCapital:String?,
+    val countryRegion: String?,
+    val countryCurrency: String?,
+    val countryFlag: String?,
+    val countryLanguage: String?
+)
