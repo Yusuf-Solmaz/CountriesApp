6621ebd40e57c0e884495253761a224ccaec6a1c
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2023-07-12 20:30:11 +0300

    View Model

--- app/src/main/java/com/yusuf/mapapp/viewmodel/CountryListViewModel.kt
+++ app/src/main/java/com/yusuf/mapapp/viewmodel/CountryListViewModel.kt
@@ -1,6 +1,12 @@
 package com.yusuf.mapapp.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.yusuf.mapapp.model.CountryModel
 
 class CountryListViewModel : ViewModel() {
+
+    val countries  = MutableLiveData<List<CountryModel>>()
+    val error  = MutableLiveData<Boolean>()
+    val countriesLoading  = MutableLiveData<Boolean>()
 }
