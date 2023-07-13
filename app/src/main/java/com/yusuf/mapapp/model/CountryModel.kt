4ff73c87fd9d7121ad54d2a6e0cfdc64f172b12b
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2023-07-13 11:40:14 +0300

    Connecting with Retrofit

--- app/src/main/java/com/yusuf/mapapp/model/CountryModel.kt
+++ app/src/main/java/com/yusuf/mapapp/model/CountryModel.kt
@@ -1,10 +1,23 @@
 package com.yusuf.mapapp.model
 
+import com.google.gson.annotations.SerializedName
+
 data class CountryModel(
+    @SerializedName(value = "name")
     val countryName : String?,
+
+    @SerializedName(value = "capital")
     val countryCapital:String?,
+
+    @SerializedName(value = "region")
     val countryRegion: String?,
+
+    @SerializedName(value = "currency")
     val countryCurrency: String?,
+
+    @SerializedName(value = "flag")
     val countryFlag: String?,
+
+    @SerializedName(value = "language")
     val countryLanguage: String?
 )
