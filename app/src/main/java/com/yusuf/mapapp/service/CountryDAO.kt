4c091ac12de5308b0fc75ee7a4cb71b47bdb818b
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2023-07-15 19:39:20 +0300

    Adding Queries

---
+++ app/src/main/java/com/yusuf/mapapp/service/CountryDAO.kt
@@ -0,0 +1,26 @@
+package com.yusuf.mapapp.service
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import com.yusuf.mapapp.model.CountryModel
+
+@Dao
+interface CountryDAO {
+
+    @Insert
+    suspend fun insertAll(vararg countries : CountryModel): List<Long>
+
+    @Query("Select * from country ")
+    suspend fun getAllCountries (): List<CountryModel>
+
+    @Query("Select * from country where id =:id")
+    suspend fun getOneCountryById (id: Int) : CountryModel
+
+    @Delete
+    suspend fun deleteCountry(country: CountryModel)
+
+    @Query("Delete from country where name= :name")
+    suspend fun deleteCountryByName(name : String)
+}
